865cd0683a855cbc0d9a2e24e5bd3eb8023dd399
David Huang <dhuang5252@gmail.com>
2021-02-25 21:47:31 -0600

    face detection optimizations

--- face_detection.py
+++ face_detection.py
@@ -2,16 +2,38 @@ import cv2, time
 
 from datacollector import DataCollector
 from dataprocessor.bpmcalc import BPMCalc
+from threading import Thread
 
+class WebCam:
+  """
+  Read frames from webcam in separate thread
+  """
+  def __init__(self, device_index):
+    self._video_capture = cv2.VideoCapture(device_index)
+    _, self._frame = self._video_capture.read()
+    self._stop = False
+  
+  def start(self):
+    Thread(target=self._update, args=()).start()
 
-# dummy function
-def calculate_bpm(timestamp, xy):
-    return 100
+  def get_frame(self):
+    return self._frame
+  
+  def stop(self):
+    self._stop = True
+
+  def _update(self):
+    while True:
+      if self._stop:
+        return
+      _, self._frame = self._video_capture.read()
 
 
 if __name__ == '__main__':
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface_improved.xml')
     video_capture = cv2.VideoCapture(0)
+    webcam = WebCam(0)
+    webcam.start()
     draw_border = True
     start_time = None
     calc_time = None
@@ -19,10 +41,10 @@ if __name__ == '__main__':
     bpm_calc = BPMCalc()
 
     while True:
-        _, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = webcam.get_frame()
 
-        faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
         for (x, y, w, h) in faces:
             if draw_border:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -33,18 +55,16 @@ if __name__ == '__main__':
                 curr_time -= start_time
             else:
                 start_time = time.time()
-                curr_time = 0
-                calc_time = curr_time
-            # This is a dummy function for now
-            bpm = calculate_bpm(curr_time, (x, y))
+                curr_time = calc_time = 0
+            print(curr_time)
+            
+            # data_collector.add(curr_time, (x, y))
+            # bpm_calc.send_data(curr_time, (x, y))
 
-            data_collector.add(curr_time, (x, y))
-            bpm_calc.send_data(curr_time, (x, y))
-
-            if(curr_time - calc_time > 2):
-                bpm = bpm_calc.calculate_bpm()
-                print(bpm)
-                calc_time = curr_time
+            # if(curr_time - calc_time > 2):
+            #     bpm = bpm_calc.calculate_bpm()
+            #     print(bpm)
+            #     calc_time = curr_time
 
         cv2.imshow('Video', frame)
 
@@ -52,5 +72,5 @@ if __name__ == '__main__':
             data_collector.write()
             break
 
-    video_capture.release()
+    webcam.stop()
     cv2.destroyAllWindows()
