6bfee25838557255a6108a9a00336a2f2b7fd8aa
David Huang <dhuang5252@gmail.com>
2021-02-27 17:16:56 -0600

    print bpm to window

--- face_detection.py
+++ face_detection.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     draw_border = True
     start_time = None
     calc_time = None
+    bpm = None
     data_collector = DataCollector()
     bpm_calc = BPMCalc()
 
@@ -48,28 +49,28 @@ if __name__ == '__main__':
         for (x, y, w, h) in faces:
             if draw_border:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            # TODO handle case where multiple "faces" are detected
-
             curr_time = time.time()
             if start_time:
                 curr_time -= start_time
             else:
                 start_time = time.time()
                 curr_time = calc_time = 0
-            # print(curr_time)
 
             data_collector.add(curr_time, (x, y))
             bpm_calc.send_data(curr_time, (x, y))
 
             if(curr_time - calc_time > 2):
                 bpm = bpm_calc.calculate_bpm()
-                print(bpm, "\tcalculation took ", (time.time() - curr_time - start_time), " seconds")
+                # print(bpm, "\tcalculation took ", (time.time() - curr_time - start_time), " seconds")
                 calc_time = curr_time
 
+        if bpm:
+          cv2.putText(frame, f'Estimated bpm: {bpm}', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        else:
+          cv2.putText(frame, f'Estimating bpm...', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            # data_collector.write()
             break
 
     webcam.stop()
